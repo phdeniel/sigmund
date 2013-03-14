8c0fa8bdb81e8aa620275e03f9b2813552a6eafd
Philippe DENIEL <philippe.deniel@cea.fr>
2013-03-14 13:08:36 +0100

    Initial commit (using Ganesha's non-regression stuff)

---
+++ run_test.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+CURDIR=`dirname $0`
+# include test framework
+. $CURDIR/test_framework.inc
+
+if [[ -r ./$CURDIR/run_test.rc ]]  ; then
+   .  ./$CURDIR/run_test.rc
+else
+  echo "  /!\\ Please make test's configuration in  $CURDIR/run_test.rc"
+  exit 1 
+fi
+
+# The script uses "su", it has to be run as root
+if [[ `id -u` != 0 ]] ; then
+  echo "  /!\\ This script must be run as root"
+  exit 1 
+fi
+
+
+######################## DEFINE TEST LIST HERE ####################
+
+# syntax: ONLY=2,3 ./run_test.sh [-j] <test_dir>
+for m in  $MODULES ; do
+  .  $CURDIR/$MODULES/testsuite.inc
+  RUN_CMD="run_$m"
+  eval $RUN_CMD
+done
+
