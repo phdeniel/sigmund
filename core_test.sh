d71a8a85c952e98abb4ad906fcc76e75d4fac70c
Philippe Deniel <philippe.deniel@cea.fr>
2013-03-22 16:03:41 +0100

    core: fixing bug in run_test.sh/core_test.sh logic

--- core_test.sh
+++ core_test.sh
@@ -1,11 +1,35 @@
-#!/bin/bash
+#!/bin/bash 
 
 CURDIR=$(dirname $(readlink -m $0))
 RUNDIR=$CURDIR
-RCFILE=$(dirname $(readlink -m $0))/run_test.rc 
 
 # include test framework
 . $CURDIR/test_framework.inc
+
+while getopts "qj" opt ; do
+        case "$opt" in
+                j)      export junit=1;;
+                q)      export quiet=1;;
+                [?])
+                        echo >&2 "Usage: $0 [-jq] [rcfile]"
+                        echo >&2 "For running a subset of tests:  ONLY=2,5 $0 [-jq] [rcfile]"
+                        exit 1;;
+        esac
+done
+shift $(($OPTIND-1))
+
+if [ -z "$1" ]; then
+    RCFILE=$(dirname $(readlink -m $0))/run_test.rc
+    #echo >&2 "Usage: $0 [-jq] [rcfile]"
+    #echo >&2 "For running a subset of tests:  ONLY=2,5 $0 [-jq] [rcfile]"
+else
+    RCFILE=$1
+fi
+
+
+if [[ -z  $RCFILE ]] ; then
+	RCFILE=$(dirname $(readlink -m $0))/run_test.rc 
+fi
 
 if [[ -r $RCFILE ]]  ; then
    .  $RCFILE
@@ -14,10 +38,16 @@ else
   exit 1 
 fi
 
+# prepare ONLY variable
+# 1,2 => ,test1,test2,
+if [[ -n $ONLY ]]; then
+    ONLY=",$ONLY"
+    ONLY=`echo $ONLY | sed -e 's/,/,test/g'`
+    ONLY="$ONLY,"
+fi
+
 export BUILD_TEST_DIR
 export RCFILE
-
-echo "$MODULES;$ONLY"
 
 ######################## DEFINE TEST LIST HERE ####################
 # Tests modules to be used
--- run_test.sh
+++ run_test.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
 
 CURDIR=$(dirname $(readlink -m $0))
-RUNDIR=$CURDIR
-# include test framework
-. $CURDIR/test_framework.inc
-
-if [ -z "$1" ]; then
-    RCFILE=$(dirname $(readlink -m $0))/run_test.rc
-    #echo >&2 "Usage: $0 [-jq] [rcfile]"
-    #echo >&2 "For running a subset of tests:  ONLY=2,5 $0 [-jq] [rcfile]"
-else
-    RCFILE=$1
-fi
-
-# prepare ONLY variable
-# 1,2 => ,test1,test2,
-if [[ -n $ONLY ]]; then
-    ONLY=",$ONLY"
-    ONLY=`echo $ONLY | sed -e 's/,/,test/g'`
-    ONLY="$ONLY,"
-fi
-
-
-if [[ -r $RCFILE ]]  ; then
-   .  $RCFILE
-else
-  echo "  /!\\ Please make test's configuration in  $RCFILE"
-  exit 1 
-fi
-
-export BUILD_TEST_DIR
 
 # The script uses "su", it has to be run as root
 if [[ `id -u` != 0 ]] ; then
@@ -45,4 +16,4 @@ if [[ ! -z $ONLY ]] ; then
   export ONLY
 fi
 
-su $TEST_USER -c "$CURDIR/core_test.sh $savedargs"
+su $TEST_USER -c "$CURDIR/core_test.sh $*"
