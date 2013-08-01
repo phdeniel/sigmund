5667915f08e9e78a236cd589658d8e0b5cf6c22c
Philippe DENIEL <philippe.deniel@cea.fr>
2013-08-01 10:36:04 +0200

    RPM: no more stupid "ln -s" is required to build rpm

--- do_rpm.sh
+++ do_rpm.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -x 
-here=`basename $PWD`
-cd ..
-ln -s $here sigmund-1
-tar cvfz sigmund.tar.gz sigmund-1/* && rpmbuild -ta sigmund.tar.gz
-cd $here
+tar --transform 's,^,sigmund-1/,' -cvz -f sigmund.tar.gz * && rpmbuild -ta sigmund.tar.gz
+rm  sigmund.tar.gz 
 
