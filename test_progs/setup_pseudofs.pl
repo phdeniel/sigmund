1160154f67ea890a88f2eb279bcc11a8c398795c
Jeremy Bongio <jbongio@us.ibm.com>
2013-08-15 13:56:11 -0400

    Adding config variables for hostname and location of FS on ganesha srever.

--- test_progs/setup_pseudofs.pl
+++ test_progs/setup_pseudofs.pl
@@ -3,13 +3,15 @@
 $CONFIG_DIR = "../ganesha_configs";
 
 sub setup_server() {
-    my ($MOUNTDIR, $PREFIX, $VERSION, $NUMBER, $FSAL) = @_;
+    my ($SERVERHOST, $MOUNTDIR, $PREFIX, $VERSION, $NUMBER, $FSAL) = @_;
 
     # create export config
     `${CONFIG_DIR}/createmanyexports.pl ${MOUNTDIR} ${PREFIX} ${VERSION} ${NUMBER} ${FSAL} > /tmp/exports.conf`;
 
-    # move export config to server and restart server
+    # backup server's current export config so we can reinstate it later.
 
+    # move export config to server and restart server
+    
 }
 
 sub setup_client() {
